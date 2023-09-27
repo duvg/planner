7152180a44a9b621dc2e7d628eef5a49a004be97
Duviel Garcia <duviel7@gmail.com>
2023-09-26 22:53:31 -0500

    [Feature] Incorporate SQLite databse and implements CRUD operations

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from routes.users import user_router
 from routes.events import event_router
+from database.connection import conn
 
 import uvicorn
 
@@ -8,6 +9,10 @@ app = FastAPI()
 
 app.include_router(user_router, prefix="/users")
 app.include_router(event_router, prefix="/events")
+
+@app.on_event("startup")
+def on_startup():
+  conn()
 
 if __name__ == "__main__":
   uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
